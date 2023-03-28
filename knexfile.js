473c7f9ec5efca4c62c857caabb05dccaeccb973
Yulia Trubacheva <yulitrub@gmail.com>
2023-03-28 15:31:57 +0100

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: "rootroot",
+      database: "db_name", // make a db in workbench first
+      charset: "utf8",
+    },
+  },
+};
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
