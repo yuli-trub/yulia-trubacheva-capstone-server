8273d28519430c972266ce8a3c0b1b5ab77d69b4
Yulia Trubacheva <yulitrub@gmail.com>
2023-04-04 22:18:50 +0100

    create PUT endpoint for profile

--- routes/profiles.js
+++ routes/profiles.js
@@ -86,4 +86,68 @@ router.get("/:id", async (req, res) => {
       .json({ error: true, message: `Could not fetch item ${profileId}` });
   }
 });
+
+router.put("/:id", async (req, res) => {
+  const profileId = req.params.id;
+
+  const { isSwiped, isFriend } = req.body;
+  try {
+    const profile = await knex("profile").where({ id: profileId }).update({
+      isSwiped,
+      isFriend,
+    });
+
+    const updatedProfile = await knex("profile")
+      .select([
+        "profile.id as id",
+        "profile.avatar_url as avatar",
+        "profile.name as name",
+        "profile.age as age",
+        "profile.bio as bio",
+        "location.location_name as location",
+        "profile.start_date as start_date",
+        "profile.end_date as end_date",
+        "profile.isFriend as isFriend",
+        "profile.isSwiped as isSwiped",
+      ])
+      .from("profile")
+      .join("location", "profile.location_id", "location.id")
+      .where({ "profile.id": profileId })
+      .groupBy("profile.id");
+
+    const events = await knex("event")
+      .select(["event.*"])
+      .from("event")
+      .innerJoin("profile_event", "event.id", "profile_event.event_id")
+      .where({ "profile_event.profile_id": profileId })
+      .orderBy("event.date", "desc");
+
+    const interests = await knex("interest")
+      .select([
+        "interest.id as interest_id",
+        "interest.interest_name as interest_name",
+      ])
+      .from("interest")
+      .innerJoin(
+        "profile_interest",
+        "interest.id",
+        "profile_interest.interest_id"
+      )
+      .where({ "profile_interest.profile_id": profileId })
+      .groupBy("interest.id");
+
+    const fullUpdatedProfile = {
+      ...updatedProfile[0],
+      events,
+      interests,
+    };
+
+    res.json(fullUpdatedProfile);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(404)
+      .json({ error: true, message: `Could not find inventory item ${invId}` });
+  }
+});
 module.exports = router;
